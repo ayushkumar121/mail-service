c86b9c15d4507d8c41a23d4548da446ff4eb31bc
Ayush Kumar <ayushkumar121@outlook.com>
2026-05-12 23:27:25 +0530

    added make

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ cp mail-service.service /etc/systemd/system/mail-service.service
 cp config.json          /etc/mail-service.json
 mkdir -p /var/mail-service/maildir
 
+make
+
 systemctl daemon-reload
 systemctl enable --now mail-service
 systemctl status --no-pager -n 5 mail-service
