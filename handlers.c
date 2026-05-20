1105880e28f2a2794189e7c9c07d6e92f718d468
Ayush Kumar <ayushkumar121@outlook.com>
2026-05-20 22:34:50 +0530

    rejecting CONNECT http method

--- handlers.c
+++ handlers.c
@@ -3,8 +3,13 @@
 #include "metrics.h"
 
 HttpResponse http_handler(const HttpRequest* request) {
-    if (sv_equal_ignore_case(request->path, SV("/metrics"))) {
+    if (sv_equal_ignore_case(request->method, SV("CONNECT"))) {
+        return http_status_response(405);
+    }
+
+    if (sv_equal_ignore_case(request->method, SV("GET")) && sv_equal_ignore_case(request->path, SV("/metrics"))) {
         return metrics_handler(request);
     }
+    
     return http_status_response(404);
 }
