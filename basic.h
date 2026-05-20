e8e546bd4c5f11a4fda904e1e1a7b0e7fbbcc223
Ayush Kumar <ayushkumar121@outlook.com>
2026-05-20 23:13:10 +0530

    Added read_crlf

--- basic.h
+++ basic.h
@@ -271,6 +271,7 @@ typedef struct {
 Error bufio_read_until(BufIO* bio, const char* terminator);
 Error bufio_read_n(BufIO* bio, size_t n);
 Error bufio_read_line(BufIO* bio);
+Error bufio_read_crlf(BufIO* bio);
 Error bufio_read_all(BufIO* bio);
 // Append to the internal write buffer without flushing. Must be paired with bufio_flush.
 void  bufio_write(BufIO* bio, String data);
